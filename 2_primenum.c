8120194bea6f5a57b19f162d1a6895f292df24fc
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-02-20 11:09:41 +0530

    02-02-2023

---
+++ 2_primenum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int num_329,count=0,i_329;
+    printf(" Enter Any positive number: ");
+    scanf("%d",&num_329);
+
+    if(num_329 == 0 || num_329 == 1)
+    count++;
+    
+    for(i_329=2; i_329<num_329; i_329++)
+    {
+        if(num_329%i_329 == 0)
+        {
+            count++;
+            break;
+        }
+    }
+    if(count == 0)
+        printf("Prime number\n");
+    else
+        printf("Not a prime number\n");
+    return 0;
+}
