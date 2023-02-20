3f476bdfce4da5bd01596a043f4e30fad04c7626
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-02-20 11:11:31 +0530

    03-02-2023

---
+++ switch-pass-fail.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int mark_329,tm_329;
+    printf("Enter your marks :");
+    scanf("%d",&mark_329);
+    tm_329=mark_329/10;
+    switch (tm_329)
+    {
+    case 9:
+        printf("The grade is O .");
+        break;
+    case 8:
+        printf("The grade is E .");
+        break;    
+    case 7:
+        printf("The grade is A .");
+        break;
+    case 6:
+        printf("The grade is B .");
+        break;
+    case 5:
+        printf("The grade is C .");
+        break;    
+    case 4:
+        printf("The grade is D .");
+        break;
+        
+        default:
+        printf("You are fail.");
+        break;
+    }
+}
