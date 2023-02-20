af58166fa1c6f9e774607a467d9a9ba657b73304
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-02-20 11:03:46 +0530

    02-09-2022

---
+++ celcious-to-farenheight.c
@@ -0,0 +1,15 @@
+//CELCIOUS TO FARENHEIGHT
+#include<stdio.h>
+int main()
+{
+    double f329,c329;
+    printf("Enter celcious temperature : ");
+    scanf("%lf",&c329);                             
+                                                       
+    f329 = (c329*1.8)+32;                            
+                                                       
+    printf("%.2lf Celcious = %.2lf Farenheight\n", c329, f329);
+    return 0;
+}
+//Name : Sudip Chakrabarty  Roll : 21053329
+//Branch : CSE              Sec : A10
