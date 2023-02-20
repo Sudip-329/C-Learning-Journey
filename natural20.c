95508293e8aa31dabad05e32e7a14e215a9f6e0b
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-02-20 13:24:48 +0530

    19-01-2023

---
+++ natural20.c
@@ -0,0 +1,12 @@
+//To print the natural numbers.
+#include<stdio.h>
+int main()
+{                                                      //Name : Sudip Chakrabarty
+    int i_329;                                         //Roll : 21053329
+    printf("\n The natural numbers are :");            //Section : A10
+                                                       //Branch : CSE
+    for(i_329=1;i_329<=20;i_329++)
+    printf("%d  ",i_329);
+    
+    return 0;
+}
